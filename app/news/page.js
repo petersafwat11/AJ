bb439c579f62a9070001c49a4de63803c0425c1a
petersafwat11 <petersafwat640@gmail.com>
2023-02-16 13:15:06 +0200

    changed some styles

--- app/news/page.js
+++ app/news/page.js
@@ -11,17 +11,19 @@ const page = () => {
         </div>
         <div className={classes["news-items"]}>
           {[1, 2, 3, 4, 5, 6].map((i) => (
-            <NewsItem
-              src={"/svg/news-photo.png"}
-              alt={"photo"}
-              heading={"Sports News Title 1"}
-              para={
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-              }
-            />
+            <>
+              <NewsItem
+                src={"/svg/news-photo.png"}
+                alt={"photo"}
+                heading={"Sports News Title 1"}
+                para={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+                }
+              />
+            </>
           ))}
         </div>
-        <div className={classes['']}></div>
+        <div className={classes[""]}></div>
       </div>
     </main>
   );
