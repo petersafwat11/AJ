9a99999053da6154b02430577ecb639107d8477c
petersafwat11 <petersafwat640@gmail.com>
2023-04-15 22:25:19 +0200

    fix build

--- app/head.js
+++ app/head.js
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
@@ -11,6 +13,10 @@ export default function Head() {
         name="viewport"
         content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no,viewport-fit=cover"
       />
+      <Script
+        type="module"
+        src="https://cdn.jsdelivr.net/npm/jwplayer-video-element@1.0/+esm"
+      ></Script>
 
       <link rel="icon" href="/favicon.ico" />
     </>
--- components/video/VideoJs.js
+++ components/video/VideoJs.js
@@ -82,16 +82,11 @@
 
 import "jwplayer-video-element";
 import Image from "next/image";
-import Script from "next/script";
 import React from "react";
 import classes from "./video.module.css";
 const VideoJs = () => {
   return (
     <div className={classes["video-wrapper"]}>
-      <Script
-        type="module"
-        src="https://cdn.jsdelivr.net/npm/jwplayer-video-element@1.0/+esm"
-      ></Script>
 
       <jwplayer-video
         controls
@@ -100,7 +95,13 @@ const VideoJs = () => {
         className={classes["video-player"]}
         src="https://cdn.jwplayer.com/players/C8YE48zj-IxzuqJ4M.html"
       ></jwplayer-video>
-      <Image className={classes['logo']} src="/LOGO.svg" alt="logo" width="94" height="73" />
+      <Image
+        className={classes["logo"]}
+        src="/LOGO.svg"
+        alt="logo"
+        width="94"
+        height="73"
+      />
     </div>
   );
 };
