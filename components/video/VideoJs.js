cf32d3f9f55f732026ab05ee6c60576b0188f803
petersafwat11 <petersafwat640@gmail.com>
2023-04-17 15:15:21 +0200

    fix

--- components/video/VideoJs.js
+++ components/video/VideoJs.js
@@ -1,12 +1,12 @@
-// "use client";
-// import "jwplayer-video-element";
+import "jwplayer-video-element";
 import React from "react";
 import classes from "./video.module.css";
+import Image from "next/image";
 const VideoJs = () => {
   return (
     <div className={classes["video-wrapper"]}>
 
-      {/* <jwplayer-video
+      <jwplayer-video
         controls
         aspectRatio="inherit"
         image="/video-background.svg"
@@ -19,7 +19,7 @@ const VideoJs = () => {
         alt="logo"
         width="94"
         height="73"
-      /> */}
+      />
     </div>
   );
 };
