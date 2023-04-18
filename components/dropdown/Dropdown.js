8da0ee8a15664fab4f14debd1bf612d861c45ea2
petersafwat11 <petersafwat640@gmail.com>
2023-04-19 01:08:04 +0200

    fixed the dropdown

--- components/dropdown/Dropdown.js
+++ components/dropdown/Dropdown.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React from "react";
 import classes from "./Dropdown.module.css";
+import {IoIosArrowUp} from 'react-icons/io'
 const Dropdown = ({ name, options, selectOption, selectedOption }) => {
   console.log(name, "name");
   return (
@@ -15,13 +16,7 @@ const Dropdown = ({ name, options, selectOption, selectedOption }) => {
         }
       >
         {name}
-        <Image
-          className={classes["drop-icon"]}
-          src="/svg/chat/down-arrow.svg"
-          alt="arrow"
-          height="11"
-          width="11"
-        />
+        <IoIosArrowUp className={classes["drop-icon"]}/>
       </div>
       <div className={classes["options"]}>
         <div className={classes["space"]}></div>
@@ -30,17 +25,11 @@ const Dropdown = ({ name, options, selectOption, selectedOption }) => {
             onClick={() => {
               selectOption({ index: index, name: name });
             }}
-            style={{
-              backgroundColor:
-                selectedOption.index == index && selectedOption.name == name
-                  ? "rgb(254, 71, 71)"
-                  : "",
-              color:
-                selectedOption.index == index && selectedOption.name == name
-                  ? "rgba(255, 255, 255, 0.8)"
-                  : "",
-            }}
-            className={classes["option"]}
+            // style={{
+              
+            // }}
+            className={selectedOption.index == index && selectedOption.name == name
+                  ?classes["option-2"]: classes["option"]}
             key={index}
           >
             {item}
