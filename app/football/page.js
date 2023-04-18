7a25bbcdfe5ac1089707be81dcc1cfbb070ad8ad
petersafwat11 <petersafwat640@gmail.com>
2023-04-18 21:20:48 +0200

    fixed

--- app/football/page.js
+++ app/football/page.js
@@ -40,127 +40,129 @@ const Page = () => {
   const selectOption = (option) => {
     setSelectedOption(option);
   };
-  const[showVideo, setshowVideo]= useState(false)
-useEffect(()=>{
-setshowVideo(true)
-},[])
+  const [showVideo, setshowVideo] = useState(false);
+  useEffect(() => {
+    setshowVideo(true);
+  }, []);
   return (
-    <div className={classes["wrapper"]}>
+    <div>
       <TopLayout />
-      <section className={classes["watch-football"]}>
-        {showShareLinks && (
-          <Popup>
-            <ShareLinks toggleShareLinks={toggleShareLinks} />
-          </Popup>
-        )}
+      <div className={classes["wrapper"]}>
+        <section className={classes["watch-football"]}>
+          {showShareLinks && (
+            <Popup>
+              <ShareLinks toggleShareLinks={toggleShareLinks} />
+            </Popup>
+          )}
 
-        {showReport && (
-          <Popup>
-            <Report toggleReport={toggleReport} />
-          </Popup>
-        )}
+          {showReport && (
+            <Popup>
+              <Report toggleReport={toggleReport} />
+            </Popup>
+          )}
 
-        {!showChat && (
-          <Image
-            onClick={toggleChat}
-            className={classes["chat-icon"]}
-            src="/svg/chat-floating.svg"
-            alt="chat"
-            width="140"
-            height="140"
-          />
-        )}
-        {showChat && (
-          <div className={classes["chat"]}>
-            <Chat toggleChat={toggleChat} />
-          </div>
-        )}
-        <div className={classes["container"]}>
-          <div className={classes["navigate"]}>
-            <Link href="/">Home</Link>
-            <span> &gt; </span>
-            <Link href="/channels">Watch</Link>
-          </div>
-
-          <WatchDetails
-            lieageImage={"/svg/watch/primier-liage.svg"}
-            lieageImageDimetions={{
-              width: { desktop: "120", tablet: "84", mobile: "35" },
-              height: { desktop: "51", tablet: "36", mobile: "35" },
-            }}
-            firstTeamImageDimentions={{
-              width: { desktop: "106", tablet: "75", mobile: "54" },
-              height: { desktop: "108", tablet: "76", mobile: "52" },
-            }}
-            secondTeamImageDimentions={{
-              width: { desktop: "80", tablet: "56", mobile: "50" },
-              height: { desktop: "110", tablet: "78", mobile: "54" },
-            }}
-            firstTeamImage={"/svg/watch/man-united.svg"}
-            firstTeamName={"Man united"}
-            seconteamImage={"/svg/watch/liverpool.svg"}
-            seconteamName={"Liverpool"}
-            date={"Aug 18 15:00"}
-            place={"Old Trafford"}
-            half={"2nd Half: 47’"}
-          />
-          <div className={classes["watch-video-wrapper"]}>
-            <div className={classes["social-icons"]}>
-              <SocialIcons
-                toggleShareLinks={toggleShareLinks}
-                toggleReport={toggleReport}
-              />
+          {!showChat && (
+            <Image
+              onClick={toggleChat}
+              className={classes["chat-icon"]}
+              src="/svg/chat-floating.svg"
+              alt="chat"
+              width="140"
+              height="140"
+            />
+          )}
+          {showChat && (
+            <div className={classes["chat"]}>
+              <Chat toggleChat={toggleChat} />
+            </div>
+          )}
+          <div className={classes["container"]}>
+            <div className={classes["navigate"]}>
+              <Link href="/">Home</Link>
+              <span> &gt; </span>
+              <Link href="/channels">Watch</Link>
             </div>
 
-            <div id="my-root-div" className={classes["watch-video"]}>
-              <PlayerContainer />
-            </div>
-            <div className={classes["watch-video-wrapper-bottom"]}>
-              <div className={classes["dropdowns"]}>
-                <Dropdown
-                  selectOption={selectOption}
-                  selectedOption={selectedOption}
-                  name={"english"}
-                  options={["1", "2", "3"]}
-                />
-                <Dropdown
-                  selectOption={selectOption}
-                  selectedOption={selectedOption}
-                  name={"العربية"}
-                  options={["1", "2", "3"]}
-                />
-                <Dropdown
-                  selectOption={selectOption}
-                  selectedOption={selectedOption}
-                  name={"espanol"}
-                  options={["1", "2", "3"]}
-                />
-                <Dropdown
-                  selectOption={selectOption}
-                  selectedOption={selectedOption}
-                  name={"more"}
-                  options={["FRENCH", "TURKISH", "PORTUGUE.."]}
+            <WatchDetails
+              lieageImage={"/svg/watch/primier-liage.svg"}
+              lieageImageDimetions={{
+                width: { desktop: "120", tablet: "84", mobile: "35" },
+                height: { desktop: "51", tablet: "36", mobile: "35" },
+              }}
+              firstTeamImageDimentions={{
+                width: { desktop: "106", tablet: "75", mobile: "54" },
+                height: { desktop: "108", tablet: "76", mobile: "52" },
+              }}
+              secondTeamImageDimentions={{
+                width: { desktop: "80", tablet: "56", mobile: "50" },
+                height: { desktop: "110", tablet: "78", mobile: "54" },
+              }}
+              firstTeamImage={"/svg/watch/man-united.svg"}
+              firstTeamName={"Man united"}
+              seconteamImage={"/svg/watch/liverpool.svg"}
+              seconteamName={"Liverpool"}
+              date={"Aug 18 15:00"}
+              place={"Old Trafford"}
+              half={"2nd Half: 47’"}
+            />
+            <div className={classes["watch-video-wrapper"]}>
+              <div className={classes["social-icons"]}>
+                <SocialIcons
+                  toggleShareLinks={toggleShareLinks}
+                  toggleReport={toggleReport}
                 />
               </div>
-              <button className={classes["extend-button"]}>EXTEND</button>
+
+              <div id="my-root-div" className={classes["watch-video"]}>
+                <PlayerContainer />
+              </div>
+              <div className={classes["watch-video-wrapper-bottom"]}>
+                <div className={classes["dropdowns"]}>
+                  <Dropdown
+                    selectOption={selectOption}
+                    selectedOption={selectedOption}
+                    name={"english"}
+                    options={["1", "2", "3"]}
+                  />
+                  <Dropdown
+                    selectOption={selectOption}
+                    selectedOption={selectedOption}
+                    name={"العربية"}
+                    options={["1", "2", "3"]}
+                  />
+                  <Dropdown
+                    selectOption={selectOption}
+                    selectedOption={selectedOption}
+                    name={"espanol"}
+                    options={["1", "2", "3"]}
+                  />
+                  <Dropdown
+                    selectOption={selectOption}
+                    selectedOption={selectedOption}
+                    name={"more"}
+                    options={["FRENCH", "TURKISH", "PORTUGUE.."]}
+                  />
+                </div>
+                <button className={classes["extend-button"]}>EXTEND</button>
+              </div>
+            </div>
+            <div className={classes["bottom"]}>
+              <div className={classes["buy-vpn"]}>
+                <ProtonVpn />
+              </div>
+              <div className={classes["takticks"]}>
+                <Takticks />
+              </div>
+              <div className={classes["casino"]}>
+                <Casino />
+              </div>
+              <div className={classes["who-will-win"]}>
+                <WhoWillWin />
+              </div>
             </div>
           </div>
-          <div className={classes["bottom"]}>
-            <div className={classes["buy-vpn"]}>
-              <ProtonVpn />
-            </div>
-            <div className={classes["takticks"]}>
-              <Takticks />
-            </div>
-            <div className={classes["casino"]}>
-              <Casino />
-            </div>
-            <div className={classes["who-will-win"]}>
-              <WhoWillWin />
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
