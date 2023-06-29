cf1b562295464e57ed16e9b3fedc2f37c50195e7
peter safwat <petersafwat640@gmail.com>
2023-06-29 06:29:53 +0200

    added baseball page

--- components/watchtaktick/baseball/Lineups.js
+++ components/watchtaktick/baseball/Lineups.js
@@ -1,10 +1,27 @@
-import React from "react";
-import LineupsDetails from "./LineupsDetails";
+import React, { useState } from "react";
+import Details from "../details/Details";
+import MatchDots from "../matchDots/MatchDots";
+import Temprature from "../temprature/Temprature";
+import Staduim from "./Staduim";
 import classes from "./lineups.module.css";
 const Lineups = () => {
+  const [option, setOption] = useState(1);
+  const changeCategory = (option) => {
+    setOption(option);
+  };
+
   return (
     <div className={classes["container"]}>
-      <LineupsDetails
+      <Staduim />
+      <MatchDots
+        options={[1, 2]}
+        selectedOption={option}
+        changeOptions={changeCategory}
+      />
+      <div className={classes["temp"]}>
+        <Temprature />
+      </div>
+      <Details
         matchDetails={[
           { left: "VENUE", right: "O2 Arena" },
           { left: "LOCATION", right: "New Delhi, India" },
---
+++ components/watchtaktick/baseball/Staduim.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import React from "react";
+import classes from "./staduim.module.css";
+
+const Staduim = () => {
+  return (
+    <div className={classes["stadium"]}>
+      {["one", "two", "three", "four", "five", "six", "seven"].map(
+        (item, index) => (
+          <div key={item} className={classes[item]}>
+            <Image
+              src="/svg/watch/baseball/player.svg"
+              alt="player-icon"
+              width="41"
+              height="45"
+            />
+            <p className={classes["player-name"]}>Lorem </p>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default Staduim;
