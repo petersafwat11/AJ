3553f88553f00cb101e9575b1e28368d6358abc2
peter safwat <petersafwat640@gmail.com>
2023-07-31 14:41:26 +0200

    made 3 pie charts

--- app/(admin)/dashboard/page.js
+++ app/(admin)/dashboard/page.js
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import ChartsWrapper from "../../../components/dashboard/stats/chartsWrapper/ChartsWrapper";
+import Chat from "../../../components/dashboard/stats/chat/Chat";
+import Feedback from "../../../components/dashboard/stats/feedback/Feedback";
+import Listings from "../../../components/dashboard/stats/listings/Listings";
+import ReportedLinks from "../../../components/dashboard/stats/reported-links/ReportedLinks";
+import classes from "./page.module.css";
 const page = () => {
   return (
-    <div>
-      dashboard
+    <div className={classes["container"]}>
+      <ChartsWrapper />
+      <div className={classes["second"]}>
+        <Listings />
+        <Chat />
+        <ReportedLinks />
+        <Feedback />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ components/dashboard/stats/lineChart/LineChart.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import classes from "./lineChart.module.css";
+export class LineChart extends Component {
+  render() {
+    return <div className={classes["container"]}></div>;
+  }
+}
+
+export default LineChart;
