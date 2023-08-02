ecaa9bfde15613507031d9b720df872b8d5e81f0
peter safwat <petersafwat640@gmail.com>
2023-08-03 01:27:30 +0200

    fix footer

--- components/dashboard/actionsButtons/ActionsButtons.js
+++ components/dashboard/actionsButtons/ActionsButtons.js
@@ -1,9 +1,23 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import classes from "./actionsButtons.module.css";
 const ActionsButtons = ({ first, second }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <div className={classes["actions"]}>
-      {first && <button className={classes["first-button"]}>{first}</button>}
+      {first && (
+        <button
+          onClick={() => {
+            first === "Create Listing" ? router.push(`/${pathname}/edit`) : "";
+          }}
+          className={classes["first-button"]}
+        >
+          {first}
+        </button>
+      )}
       {second && <button className={classes["second-button"]}>{second}</button>}
     </div>
   );
--- components/dashboard/table/Table.js
+++ components/dashboard/table/Table.js
@@ -1,8 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { BiSearch } from "react-icons/bi";
 import { BsFlagFill } from "react-icons/bs";
 import classes from "./table.module.css";
 const Table = () => {
+  const router = useRouter();
   return (
     <div className={classes["table"]}>
       <div className={classes["search-wrapper"]}>
@@ -10,28 +13,40 @@ const Table = () => {
         <BiSearch className={classes["search-icon"]} />
       </div>
       <div className={classes["table-header"]}>
-        <p className={classes["table-cell"]}>No</p>
-        <p className={classes["table-cell"]}>Feedback Message</p>
-        <p className={classes["table-cell"]}>Flag</p>
+        <span></span>
+        <p className={classes["table-cell"]}>ID</p>
+        <p className={classes["table-cell"]}>Team 1</p>
+        <p className={classes["table-cell"]}>Team 2</p>
+        <p className={classes["table-cell"]}>League</p>
         <p className={classes["table-cell"]}>Date/Time</p>
+        <p className={classes["table-cell"]}>Sport</p>
+        <p className={classes["table-cell"]}>Hot Match</p>
+        <p className={classes["table-cell"]}>Action </p>
       </div>
-      {[1, 2, 3, 4, 5].map((item, index) => (
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
         <div key={index} className={classes["table-row"]}>
-          <p className={classes["table-cell"]}>533</p>
-          <p className={classes["table-cell"]}>
-            Our planet is in danger. The food system is depleting our soils,
-            changing our climate and driving biodiversity loss. Poorer
-            communities at home and in the Global South are bearing the brunt of
-            the destruction wreaked by agribusiness corporations. But it
-            doesn&#39; t have to be like this.
-          </p>
-          <div className={classes["flag"]}>
-            <BsFlagFill />
-          </div>
+          <input type="checkbox" className={classes["checkbox"]} />
+          <p className={classes["table-cell"]}>{item}</p>
+          <p className={classes["table-cell"]}>Man Utd</p>
+          <p className={classes["table-cell"]}>Liverpool</p>
+          <p className={classes["table-cell"]}>Premier League</p>
           <div className={classes["date-and-time"]}>
             <p>12/07/23</p>
             <p>23:12</p>
           </div>
+          <p className={classes["table-cell"]}>Football</p>
+
+          <div className={classes["flag"]}>
+            <BsFlagFill />
+          </div>
+          <button
+            onClick={() => {
+              router.push("/dashboard/sports-listings/edit");
+            }}
+            className={classes["edit-button"]}
+          >
+            Edit{" "}
+          </button>
         </div>
       ))}
     </div>
