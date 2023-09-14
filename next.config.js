e04396d34db69eb58ef29dbea69da0ef68e5e86a
peter safwat <petersafwat640@gmail.com>
2023-09-14 03:34:33 +0200

    seitch server

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    // BACKEND_SERVER: "https://long-cummerbund-slug.cyclic.app/api",
-    // STATIC_SERVER: "https://long-cummerbund-slug.cyclic.app",
+    BACKEND_SERVER: "https://long-cummerbund-slug.cyclic.app/api",
+    STATIC_SERVER: "https://long-cummerbund-slug.cyclic.app",
 
-    BACKEND_SERVER: "http://localhost:8000/api",
-    STATIC_SERVER: "http://localhost:8000",
+    // BACKEND_SERVER: "http://localhost:8000/api",
+    // STATIC_SERVER: "http://localhost:8000",
   },
   images: {
     domains: ["long-cummerbund-slug.cyclic.app", "localhost"],
