a792082543ebea9d62835e604188281d4b9a9c5e
peter safwat <petersafwat640@gmail.com>
2023-09-23 19:42:50 +0200

    switch servers

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const nextConfig = {
     scrollRestoration: false,
   },
   env: {
-    // BACKEND_SERVER: "https://jealous-cow-jewelry.cyclic.cloud/api",
-    // STATIC_SERVER: "https://jealous-cow-jewelry.cyclic.cloud",
-    // FRONTEND_SERVER: "https://aj-umber.vercel.app",
-    FRONTEND_SERVER: "http://localhost:3000",
-    BACKEND_SERVER: "http://localhost:8000/api",
-    STATIC_SERVER: "http://localhost:8000",
+    BACKEND_SERVER: "https://jealous-cow-jewelry.cyclic.cloud/api",
+    STATIC_SERVER: "https://jealous-cow-jewelry.cyclic.cloud",
+    FRONTEND_SERVER: "https://aj-umber.vercel.app",
+    // FRONTEND_SERVER: "http://localhost:3000",
+    // BACKEND_SERVER: "http://localhost:8000/api",
+    // STATIC_SERVER: "http://localhost:8000",
   },
   images: {
     domains: ["jealous-cow-jewelry.cyclic.cloud", "localhost"],
