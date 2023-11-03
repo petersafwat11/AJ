bb6768c1a918b242b86ecf9b7340f46a6572c8dc
peter safwat <petersafwat640@gmail.com>
2023-11-03 18:49:30 +0200

    fixed build

--- next.config.js
+++ next.config.js
@@ -13,8 +13,8 @@ const nextConfig = {
     // FRONTEND_SERVER: "http://localhost:3000",
     // BACKEND_SERVER: "http://localhost:8000/api",
     // STATIC_SERVER: "http://localhost:8000",
-    // MONGODB_URI:
-    //   "mongodb+srv://petersafwat:YzegQ0QJlV0ljR0B@cluster0.dsafm2w.mongodb.net/AJSPORTS?retryWrites=true&w=majority",
+    MONGODB_URI:
+      "mongodb+srv://petersafwat:YzegQ0QJlV0ljR0B@cluster0.dsafm2w.mongodb.net/AJSPORTS?retryWrites=true&w=majority",
     // GOOGLE_CLIENT_ID:
     //   "852941662667-467i5p7bqcgokabl1rjgi0j048vm6mev.apps.googleusercontent.com",
     // GOOGLE_CLIENT_SECRET: "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",
